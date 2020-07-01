954520b6c897410a11bc390f0348de788187194f
Karunesh Maisalge <karuneshmaisalge@Karuneshs-MacBook-Air.local>
2020-07-01 08:33:12 +0530

    Spring Security

---
+++ spring-security-jpa/src/test/java/com/karunesh/springsecurityjpa/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karunesh.springsecurityjpa.springsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
